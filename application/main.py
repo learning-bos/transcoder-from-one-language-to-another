b3c0c3dc17d37d26390a2be445ff36d629503c2b
learning-bos <pezzodimerda2024@gmail.com>
2024-11-10 21:22:57 +0100

    added recognize functionality and todos to further implement

--- application/main.py
+++ application/main.py
@@ -1,23 +1,29 @@
 import streamlit as st
-from transformers import pipeline
+from utils import set_session, is_valid_json
+
+st.session_state = set_session(st.session_state)
 
 # Upload file
 uploaded_file = st.file_uploader("Upload your file")
 
 if uploaded_file is not None:
+    chain = st.session_state.chain
+    messages = st.session_state.messages
     # Read file content
-    content = uploaded_file.read().decode("utf-8")
+    text_file = uploaded_file.read().decode("utf-8")
 
-    # Display file content (optional)
-    st.text("File Content:")
-    st.text(content)
+    # Call llm with instruction chatprompttemplate
+    with st.chat_message("assistant"):
 
-    # Load a pre-trained LLM for programming language detection
-    classifier = pipeline("text-classification", model="xlm-roberta-base")
+        response = chain.invoke(text_file).content
 
-    # Predict the programming language
-    prediction = classifier(content[:512])  # Limiting to the first 512 tokens
+        if not is_valid_json(response):
+            response = "{ 'programming_language': 'unknown' }"    
+        st.markdown(response)
+        messages.append({"role": "assistant", "content": response})
 
-    # Display the prediction
-    st.text("Predicted Programming Language:")
-    st.write(prediction)
+        # TODO: after loading the file do not display the browse anymore until you finish with this
+        # TODO: let the user select the language to convert the provided file
+        # TODO: let the user decide whether the conversion is okay or is not
+
+
